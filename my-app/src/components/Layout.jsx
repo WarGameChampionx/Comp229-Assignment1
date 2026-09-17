9a5a8acc174155d26638e1cef1afdb264c70b956
Matthew Ouellette <MatthewJMOuellette@Gmail.com>
2026-09-17 19:41:06 -0400

    Trying to fix capitalization again

--- my-app/src/components/Layout.jsx
+++ my-app/src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../assets/Logo.png'
+import logo from '../assets/logo.png'
 
 export default function Layout() {
   return (
